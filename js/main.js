8c4817d127be0bca9d0ae20652af5ea6a1090f4f
Mosho1 <sharonrolel@gmail.com>
2014-04-20 01:41:20 -0400

    wow such space

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ var enemySpeed = 20;
 var circleSpd = 3;
 var circleAcc = -0.05;
 var maxAcc = -0.025;
-var score = 124513253215252;
+var score = 0;
 var posX, posY;
 var grpId = 1;
 var gameOver = false;
